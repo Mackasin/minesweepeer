81ea11b52b3f9c7727b450846a8bbbda742d8396
Mackasin <104722368+Mackasin@users.noreply.github.com>
2023-06-16 17:26:58 +0300

    Delete Moveable.cpp

--- Moveable.cpp
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-#include <wincon.h>
-#include "../Headers/Moveable.h"
-
-
-void Moveable::gotoxy(int x, int y) {
-    COORD p = {static_cast<SHORT>(x), static_cast<SHORT>(y)};
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
-}
